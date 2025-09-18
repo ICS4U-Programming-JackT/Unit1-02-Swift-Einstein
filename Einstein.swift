7c037410116d59992e9d45381b623116688cd90f
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-09-18 14:33:25 +0000

    Created einstein equation program in swift

---
+++ Einstein.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+let SpeedOfLight:Double = 2.998 * pow(10, 8)
+
+// Error case for the input conversion failing
+enum DataError: Error {
+    case invalidInput
+}
+
+// Function to calculate the energy released
+func main() {
+    // User input
+    print("Please enter the mass of your object (in kg):")
+    
+    // Get input
+    if let mass = readLine() {
+        
+        // Try catch, throw errors if input is incorrect
+        do {
+            //Convert mass to double
+            guard let massDouble = Double(mass) else {
+                throw DataError.invalidInput
+            }
+            // Check if mass double is above zero
+            if massDouble > 0 {
+                    let energy = massDouble * pow(SpeedOfLight, 2)
+                    print("The energy released by your object "
+                    + "is \(energy) J (Joules)")
+            } else {
+                // Warn that input was not a positive number
+                print("Error: Mass must be a positive number.")
+            }
+
+
+        } catch DataError.invalidInput {
+            // Catches the error thrown
+            print("Error: Please enter a valid number.")
+        } catch {
+            print("An unexpected error occurred: \(error)")
+        }
+    } else {
+        // Warn that input could not be read
+        print("Error: Could not read input.")
+    }
+}
+
+main()
